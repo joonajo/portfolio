73fe5d4feff8b819a0d52b93a706872f23ec6450
Joona Joenpolvi <joona.joenpolvi@wolt.com>
2022-07-28 16:03:00 +0300

    Set up stuff

--- pages/index/index.tsx
+++ pages/index/index.tsx
@@ -1,18 +1,14 @@
-import React from 'react'
+import React from "react";
 
-import { NextPage } from 'next'
-import Home from '../../containers/Home/Home'
-
-const css = require('./index.module.css')
+import { NextPage } from "next";
+import Home from "../../containers/Home/Home";
 
 const Index: NextPage = (): JSX.Element => {
-    React.useEffect(() => {
-        document.title = "Portfolio // Home"
-    }, [])
+  React.useEffect(() => {
+    document.title = "Portfolio // Home";
+  }, []);
 
-    return (
-        <Home />
-    )
-}
+  return <Home />;
+};
 
-export default Index
+export default Index;
